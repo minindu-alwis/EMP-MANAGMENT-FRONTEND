5a34247d5969891b4b5546a6a3d1927d93ae1fad
minindu-alwis <minindualwis@gmail.com>
2025-04-29 17:03:48 +0530

    Add HomeComponent with routing, styles, and template; update dependencies

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { EmployeeListComponent } from './components/employee-list/employee-list.component';
 import { EmployeeFormComponent } from './components/employee-form/employee-form.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'employees', pathMatch: 'full' },
+  { path: '', component:HomeComponent },
   { path: 'employees', component: EmployeeListComponent },
   { path: 'employees/new', component: EmployeeFormComponent },
   { path: 'employees/edit/:id', component: EmployeeFormComponent },
